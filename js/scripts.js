5c7ba7a9b4882454f0c1c3658f49f9c2f20a70ea
Grace Lee <gracexsnoopy@gmail.com>
2022-08-12 10:02:19 -0700

    create function called inputs and set variables to values from the form within

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,15 @@ window.onload = function() {
   let form = document.querySelector("form");
   form.onsubmit = function(event) {
     event.preventDefault();
+    inputs()
   };
+}
+
+function inputs () {
+  const firstName = document.getElementById("first-name").value;
+  const lastName = document.getElementById("last-name").value;
+  const creation = document.querySelector("input[name='creation']:checked").value;
+  const level = document.getElementById("level").value;
+  const studyMethod = document.querySelector("input[name='study-method']:checked").value;
+  const connections = document.getElementById("connections").value;
 }
